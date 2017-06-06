0167ce3911ce8aaeefd7a3f06a1b1dd6b3fe65de
AntonS <sollfrank-anton@outlook.de>
2017-06-06 14:44:32 +0200

    Test Commit

--- src/de/bsinfo/Spielfiguren/Spieler.java
+++ src/de/bsinfo/Spielfiguren/Spieler.java
@@ -2,18 +2,62 @@ package de.bsinfo.Spielfiguren;
 
 import de.bsinfo.Items.Item;
 
-public abstract class Spieler extends GameObject{
-	
+public abstract class Spieler extends GameObject {
+
 	String name;
 	Item kopf;
 	Item waffe;
 	Item koerper;
-	Item Stiefel;
+	Item stiefel;
 
-	public Spieler(int atk, int def, int agi, int hp) {
-		super(atk, def, agi, hp, hp);
+	public Spieler(int atk, int def, int agi, int fahAtk, int hp, Item kopf, Item waffe, Item koerper, Item stiefel,
+			String name) {
+		super(atk, def, agi, fahAtk, hp);
+		this.name = name;
+		this.kopf = kopf;
+		this.waffe = waffe;
+		this.koerper = koerper;
+		this.stiefel = stiefel;
 	}
-	
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Item getKopf() {
+		return kopf;
+	}
+
+	public void setKopf(Item kopf) {
+		this.kopf = kopf;
+	}
+
+	public Item getWaffe() {
+		return waffe;
+	}
+
+	public void setWaffe(Item waffe) {
+		this.waffe = waffe;
+	}
+
+	public Item getKoerper() {
+		return koerper;
+	}
+
+	public void setKoerper(Item koerper) {
+		this.koerper = koerper;
+	}
+
+	public Item getStiefel() {
+		return stiefel;
+	}
+
+	public void setStiefel(Item stiefel) {
+		this.stiefel = stiefel;
+	}
 
 }
